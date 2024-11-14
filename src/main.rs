2e7e6779399f7a14a1e45665dc3c4b702f1076c1
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-11-14 15:34:45 -0800

    while loops and vectors

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,29 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let correct = rand::thread_rng().gen_range(1..=10);
-    println!("correct: {correct}");
-    println!("Hey, guess a number 1 - 10:");
+    let mut how_many = String::new();
+    println!("How many random numbers do you want to guess?");
+    io::stdin()
+        .read_line(&mut how_many)
+        .expect("Error reading input");
 
-    // error handling for invalid guess
-    loop {
+    let num_guesses: u8 = how_many.trim().parse().expect("Error reading input");
+
+    let mut correct = Vec::new();
+
+    for _ in 0..num_guesses {
+        correct.push(rand::thread_rng().gen_range(1..=10));
+    }
+
+    println!("{correct:?}");
+
+    // let correct = rand::thread_rng().gen_range(1..=10);
+    // println!("correct: {correct}");
+
+    let mut guesses_made = 0;
+    println!("Hey, guess a number 1 - 10:");
+    // add while loop
+    while guesses_made < num_guesses {
         let mut guess = String::new();
         // read line into string buf num
         io::stdin().read_line(&mut guess);
@@ -24,15 +41,49 @@ fn main() {
 
         // USING match
         // removed mut added uint32 cmp
-        match guess.cmp(&correct) {
+        match guess.cmp(&correct[guesses_made as usize]) {
             Ordering::Greater => println!("You guessed too high"),
             Ordering::Less => println!("You guessed too low"),
             Ordering::Equal => {
                 println!("You guessed the correct number");
-                break;
+                guesses_made += 1;
+                if guesses_made < num_guesses {
+                    println!("Let's now try the next number!")
+                }
             }
         };
     }
+    println!("Thanks for playing! The correct numbers were:");
+    for item in &correct {
+        println!("{item}")
+    }
+
+    // // error handling for invalid guess
+    // loop {
+    //     let mut guess = String::new();
+    //     // read line into string buf num
+    //     io::stdin().read_line(&mut guess);
+
+    //     // guess check if valid int
+    //     let guess: u32 = match guess.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(e) => {
+    //             println!("Error with parse, try again");
+    //             continue;
+    //         }
+    //     };
+
+    //     // USING match
+    //     // removed mut added uint32 cmp
+    //     match guess.cmp(&correct) {
+    //         Ordering::Greater => println!("You guessed too high"),
+    //         Ordering::Less => println!("You guessed too low"),
+    //         Ordering::Equal => {
+    //             println!("You guessed the correct number");
+    //             break;
+    //         }
+    //     };
+    // }
 
     // // add loop
     // loop {
